83f6d822d155af7fba10490af5f58a2db9c72f49
Lul-Abdifan <tasanbannu@gmail.com>
2023-08-09 05:36:14 -0400

    Fix eslint errors

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,41 +1,38 @@
 class FoodsController < ApplicationController
-    def index
-        @foods = current_user.foods
+  def index
+    @foods = current_user.foods
+  end
+
+  def show; end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user_id = current_user.id
+    if @food.save
+      flash[:notice] = 'New Food successfully added!'
+      redirect_to foods_path
+    else
+      flash[:alert] = 'Fill again'
+      render :new
     end
+  end
 
-     def show;
-     end
-     
-    def new
-        @food = Food.new
+  def destroy
+    @food = Food.find(params[:id])
+    if @food.destroy
+      flash[:notice] = 'Food successfully deleted!'
+    else
+      flash[:alert] = 'Food is not deleted!'
     end
+  end
 
-    def create
-        @food = Food.new(food_params)
-        @food.user_id = current_user.id
-        if @food.save
-            flash[:notice] = 'New Food successfully added!'
-            redirect_to foods_path
-        else
-            flash[:alert] = 'Fill again'
-            render :new
-        end
-    end
+  private
 
-    def destroy
-        @food = Food.find(params[:id])
-        if @food.destroy
-            flash[:notice] = 'Food successfully deleted!'
-            else
-            flash[:alert] = 'Food is not deleted!'
-            end
-    end
-
-
-    private 
-
-    def food_params
-        params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
-      end
-      
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
