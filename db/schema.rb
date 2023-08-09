9607ef8fa49b8a5b3efbaa6ac0281555e9d4dafc
Lul-Abdifan <tasanbannu@gmail.com>
2023-08-09 04:55:38 -0400

    Connect the rails with local database

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,15 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_08_08_134204) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.string "measurement_unit"
     t.integer "price"
     t.integer "quantity"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_foods_on_user_id"
