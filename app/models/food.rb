8a51e4103e7d752acb20dbdf65ac7d55057d98ea
Lul-Abdifan <tasanbannu@gmail.com>
2023-08-10 04:53:23 -0400

    Add recipefood model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,7 @@
 class Food < ApplicationRecord
   belongs_to :user
-
+  has_many :recipe_foods
+  
   validates :name, :quantity, :price, :measurement_unit, presence: true
   validates :quantity, :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
