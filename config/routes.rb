1851b2eb8f37685529aa8b19be43fee59ef6f5e1
Chritelle-12 <mariechristellenirere@gmail.com>
2023-08-10 07:47:01 +0200

    Add details page functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  resources :recipes
+  resources :recipes do
+    resources :foods, only: [:new, :create]
+  end
+  
   devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   # root "users#index"
+  
   resources :foods, only: [:new, :create, :index, :destroy]
 end
